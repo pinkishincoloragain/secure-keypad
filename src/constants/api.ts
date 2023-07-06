658af80d5b82940f3724c281d80f6ab731c509db
pinkishincoloragain <smb1103@gmail.com>
2023-07-07 01:58:12 +0900

    feat: added fetch util func

---
+++ src/constants/api.ts
@@ -0,0 +1,2 @@
+export const USERS_API_ENDPOINT = "https://localhost:8080/api/users";
+export const USER_API_ENDPOINT = "https://localhost:8080/api/user";
---
+++ src/utils/fetch.ts
@@ -0,0 +1,25 @@
+import {USER_API_ENDPOINT, USERS_API_ENDPOINT} from "../constants/api";
+import axios from "axios";
+
+const fetchAllUsers = async () => {
+    const response = await axios.get(USERS_API_ENDPOINT);
+
+    if (response.status !== 200) {
+        throw new Error("Something went wrong");
+    } else {
+        return response.data;
+    }
+}
+
+export const fetchUser = async (id: string) => {
+    const response = await axios.get(`${USER_API_ENDPOINT}/${id}`);
+
+    if (response.status !== 200) {
+        throw new Error("Something went wrong");
+    } else {
+        return response.data;
+    }
+
+}
+
+export default fetchAllUsers;
